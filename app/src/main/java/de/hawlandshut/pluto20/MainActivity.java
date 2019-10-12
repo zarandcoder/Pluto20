f427db9f72204e112aaf4838290e2a6c405d0f30
Vadim Midav <vzaripov@gmx.de>
2019-10-12 17:28:07 +0200

    Initial commit

--- app/src/main/java/de/hawlandshut/pluto20/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto20/MainActivity.java
@@ -14,10 +14,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "oncleate called");
+        Log.d(TAG, "onCreate() called");
 
         //Small comment on the bottom appears for some seconds
         Toast.makeText(getApplicationContext(), "Konto wurde angelegt",
                 Toast.LENGTH_SHORT).show();
+    }
+
+    //Methods from the Application Lifecycle
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
     }
 }
---
+++ app/src/main/java/de/hawlandshut/pluto20/PostActivity.java
@@ -0,0 +1,14 @@
+package de.hawlandshut.pluto20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
